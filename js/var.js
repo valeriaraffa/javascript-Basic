2349b6e70e29087975409611360629c997317102
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-23 20:51:25 +0200

    change the value in a variable

--- js/var.js
+++ js/var.js
@@ -2,3 +2,11 @@ var message = "Hello!";
 console.log(message+ " from var.js");
 message ="Hello from JavaScript Basics";
 console.log(message);
+
+var score =0;
+score = score +10;
+console.log(score);
+
+var bonusPts =100;
+var finalScore = score + bonusPts;
+console.log(finalScore);
