6273e67d142d279682255adfb30821b5446478cd
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-24 03:32:16 +0200

    template literals

---
+++ js/template-literals.js
@@ -0,0 +1,19 @@
+const name = prompt('Whats your name?');
+// const message = "Hello " + name;
+
+
+const message = `Hello, ${name}. Welcome to my music site. I'm happy
+ that you came by to visit, ${name}. 
+ Please come again and listen to more music!`;
+
+ console.log(message);
+
+ /**
+  * define template Literals with backticks
+  * a dollar sign with curly braces marks the dynamic values in a template literal.
+  * 
+  * interpolation:
+  * Whatever's inside the curly braces gets evaluated by the JavaScript engine and
+  * added to the string. his process is known as interpolation.
+  * 
+  */
