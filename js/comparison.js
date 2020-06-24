049e179d06c4eda8583545e76ec265f78d8ac191
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-24 22:33:49 +0200

    comparison operators

---
+++ js/comparison.js
@@ -0,0 +1,19 @@
+/**
+ * Equality Operator (==)
+ * Tests if two values are the same
+ */
+('3' == 3) //True, the browser converts the string to a number befor comparing them
+('' == 0) // True, 
+ /**
+  * Strict Equality Operator (===)
+  * Compares the types as well as the value
+  */
+('3' === 3) // False, not the same type of value , one value is a string and the other is a number
+('' == 0) // False
+
+/**
+ * Logical NOT operator (!)
+ * != not equal to
+ * !== the strict inquality operator with two equal sign, for same reason
+ */
+(10 !== 9) //true
