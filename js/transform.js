eed377b8b23924fb0d2a9ddf26feb9f4d087b77a
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-24 02:20:34 +0200

    transform and manipulate strings

---
+++ js/transform.js
@@ -0,0 +1,22 @@
+const passphrase = 'I have Spoken';
+console.log( passphrase.length );
+console.log("This is my String".length);
+console.log(passphrase.toLowerCase());
+
+/**
+ * When you use a string,the JavaScript engine treats it as an object behind the scenes.
+ * 
+ * strings are treated as what are called objects.
+ * 
+ * So the string value stored in the passphrase variable is considered
+ * an object, and different types of objects have different properties.
+ * 
+ * you can access a property of an object using a dot.
+ */
+
+ /**
+  * A property is kind of like a variable, it holds information.
+  * A string's length property, for example, contains a number,
+  * the number of characters inside the string.
+  * You can use the length property on any string,
+  */
