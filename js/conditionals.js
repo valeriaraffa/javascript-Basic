e0747eea946a0ce93d755938de23ffefbf974e87
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-24 04:42:15 +0200

    conditional statements (if else)

---
+++ js/conditionals.js
@@ -0,0 +1,7 @@
+const answer = prompt('Which planet is closest to the sun?');
+
+if( answer.toUpperCase() === 'Mercury') {
+    console.log("That's correct");
+} else {
+    console.log("Sorry thats incorrect");
+}
