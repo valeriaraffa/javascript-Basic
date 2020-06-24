fb4bb516ac5bc079bbd17f8f9b35fe80d3365510
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-24 03:08:23 +0200

    combine strings

---
+++ js/combine.js
@@ -0,0 +1,15 @@
+const name = prompt("What is your name?");
+let message = " Hello "+ name + " Welcome to my music site. "; 
+message = message + "I'm so happy that you cam by to vistit, ";
+message+= name;
+message += ". Feel free to come again and listen to more music!";
+console.log(message);
+
+/**
+ * There are different techniques for combining strings in JavaScript.
+ * First  how to add strings together using a process
+ * called concatenation
+ * 
+ * String concatenation is the process of combining or
+ * adding two or more strings together to create a bigger string
+ */
