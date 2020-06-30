6b33e558f7bb67732d37e901c969e0961dc960c7
Valeria Raffa <valeria.raffa@outlook.de>
2020-06-30 23:54:45 +0200

    Treating Strings as Numbers

--- js/string-to-number.js
+++ js/string-to-number.js
@@ -1,3 +1,6 @@
 const HTMLBadges = prompt('How many HTML badges do you have?');
 const CSSBadges = prompt('How many CSS badges do you have?');
+// prompt method returns what the user types as a string
 
+const totalBadges = HTMLBadges+CSSBadges;
+console.log(typeof totalBadges, typeof CSSBadges);// output: strings
